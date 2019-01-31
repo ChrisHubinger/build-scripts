359b21adcb52050c3c88f0f9568fdbb4aaef869f
Christian Hubinger <christian@trigo.at>
2019-01-31 23:36:33 +0100

    adfs

--- configure.sh
+++ configure.sh
@@ -4,4 +4,4 @@ rm -f Jenkinsfile
 echo "trigo.build(\"app\", ...)" > ../../Jenkinsfile
 
 rm -f Makefile
-echo "include(\"node_modules/build-scripts/Makefile.node-app.common\")" > ../../Makefile
+echo "include ./node_modules/build-scripts/Makefile.node-app.common" > ../../Makefile
