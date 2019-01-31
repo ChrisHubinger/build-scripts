2dd53e4bada234bf2d091b922ddb95d0557b861a
Christian Hubinger <christian@trigo.at>
2019-01-31 23:19:45 +0100

    asd

---
+++ Makefile
@@ -0,0 +1,1 @@
+include("node_modules/build-scripts/Makefile.node-app.common")
---
+++ configure.sh
@@ -0,0 +1,9 @@
+# Parse name & techstack from ../../gems.json
+
+set -x
+rm -f Jenkinsfile
+env | sort
+echo "trigo.build(\"app\", ...)" > ./Jenkinsfile
+
+rm -f Makefile
+echo "include(\"node_modules/build-scripts/Makefile.node-app.common\")" > ./Makefile
